a9cfb2957071d91f0a5216b1dacc6dc715a65143
mrdb303 <dannybrien@ntlworld.com>
2023-12-19 12:22:30 +0000

    updated url on client

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 //console.log("test");
 
 async function getMessage() {
-  const response = await fetch("http://localhost:8080/message");
+  const response = await fetch("https://mini-stack-server.onrender.com/");
 
   // turn the response into json
 
